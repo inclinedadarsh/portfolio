b6f65541ac993047f8ada8731fd47a3b4b5c0db3
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-11-14 01:37:58 +0530

    feat: :lipstick: add colors and typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
 
 export const metadata: Metadata = {
   title: "Adarsh Dubey • Web Developer & UX Designer",
@@ -17,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary--foreground)",
+        },
+
+        muted: {
+          1: "var(--muted-1)",
+          2: "var(--muted-2)",
+          foreground: "var(--muted--foreground)",
+        },
+
+        white: "var(--white)",
+        transparent: "var(--transparent)",
+
+        green: "var(--green)",
+      },
+      fontFamily: {
+        inter: "var(--font-inter)",
+        "space-grotesk": "var(--font-space-grotesk)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
