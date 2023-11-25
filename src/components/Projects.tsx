0d550d6fb2f3c7c8f0ac21f0bd912eb1d8bb699f
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-11-25 23:19:40 +0530

    feat(projects): :construction: add Badge and SectionTitle component and use them in new Projects component
    Abstracted the Badge component and add a new abstracted SectionTitle and use them in Projects component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "./SectionTitle";
+
+const Projects = () => {
+  return (
+    <div className="view-container mt-24">
+      <SectionTitle badges={["design", "dev"]}>Selected Projects</SectionTitle>
+    </div>
+  );
+};
+
+export default Projects;
