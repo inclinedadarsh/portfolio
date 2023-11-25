cc95da4f8ad77262bb882788becc51a2f1adc51e
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2023-11-25 21:27:25 +0530

    feat(font): Update font styles and add (#4)
    GeneralSans-Variable.ttf

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="view-container mt-16 md:mt-20">
-      <p className="font-space-grotesk text-primary-light text-base md:text-xl">
+      <p className="font-mono text-primary-light text-base md:text-xl">
         Hi, I'm Adarsh Dubey.
       </p>
-      <h1 className="font-bold text-3xl md:text-5xl lg:text-[66px] flex flex-col mt-5 leading-snug">
+      <h1 className="font-semibold text-3xl md:text-5xl lg:text-[66px] flex flex-col mt-5 leading-snug">
         <span className="text-muted-white-2">I help startups with</span>
-        <span className="text-muted-white-1 md:mt-4">
+        <span className="text-muted-white-1 md:mt-2">
           purpose driven websites.
         </span>
       </h1>
-      <p className="max-w-xl text-base lg:text-xl text-muted-foreground font-light mt-10 leading-normal">
+      <p className="max-w-xl text-base lg:text-xl text-muted-foreground mt-10 leading-normal">
         Passionate freelance web developer and designer, crafting functional
         websites for startups. Dedicated open-source contributor and community
         builder.
