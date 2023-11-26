fbbc4f7dc13eefe4c6d94720bdd2401b0b977c1f
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-11-27 01:22:19 +0530

    feat(projects): :sparkles: add ProjectCard component and use it in Projects component
    add image assets

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,81 @@
+import Image, { StaticImageData } from "next/image";
+
+import Link from "next/link";
+import { ExternalLink, Github } from "lucide-react";
+
+interface ProjectCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string;
+  scope: string;
+  description: string;
+  image: StaticImageData;
+  imageAlt: string;
+  sourceLink?: string;
+  liveLink?: string;
+  technologies: string[];
+  className?: string;
+}
+
+const ProjectCard = ({
+  title,
+  scope,
+  description,
+  image,
+  imageAlt,
+  sourceLink,
+  liveLink,
+  technologies,
+  className,
+}: ProjectCardProps) => {
+  return (
+    <div
+      className={`flex flex-col md:flex-row gap-5 p-4 md:p-7 border-2 border-muted-1 border-dashed rounded-lg ${className}`}
+    >
+      <div className="flex-1">
+        <Image src={image} alt={imageAlt} className="rounded w-full" />
+      </div>
+      <div className="flex-1">
+        <div className="flex justify-between">
+          <h3 className="text-2xl md:text-3xl font-semibold text-muted-white-1">
+            {title}
+          </h3>
+          <div className="flex gap-4">
+            {sourceLink && (
+              <Link
+                className="p-2.5 md:p-3 rounded-full bg-muted-2 hover:bg-muted-1 focus-visible:bg-muted-1 transition-colors"
+                href={sourceLink}
+              >
+                <Github width={20} height={20} strokeWidth={1.5} />
+              </Link>
+            )}
+            {liveLink && (
+              <Link
+                className="p-2.5 md:p-3 rounded-full bg-muted-2 hover:bg-muted-1 focus-visible:bg-muted-1 transition-colors"
+                href={liveLink}
+              >
+                <ExternalLink width={20} height={20} strokeWidth={1.5} />
+              </Link>
+            )}
+          </div>
+        </div>
+        <p className="uppercase tracking-wider font-mono text-muted-white-2">
+          {scope}
+        </p>
+        <p className="text-base md:text-xl leading-normal mt-5">
+          {description}
+        </p>
+        <div className="flex flex-wrap gap-3 font-mono mt-5">
+          {technologies.map((technology) => (
+            <div
+              className="text-xs md:text-sm px-2 py-1 bg-muted-1 rounded-full text-muted-white-2"
+              key={technology}
+            >
+              {technology}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,64 @@
+import Link from "next/link";
+import ProjectCard from "./ProjectCard";
 import SectionTitle from "./SectionTitle";
+
+import { cipheredBanner, timeloomBanner, unlinkedBanner } from "@/assets";
 
 const Projects = () => {
   return (
-    <div className="view-container mt-24">
+    <section className="view-container mt-24">
       <SectionTitle badges={["design", "dev"]}>Selected Projects</SectionTitle>
-    </div>
+      <div className="space-y-8">
+        <ProjectCard
+          title="Timeloom"
+          scope="Design + Development"
+          description="Timeloom helps you to document your technical journey in an activity timeline like page. You can create your own page and craft it as you like."
+          image={timeloomBanner}
+          imageAlt="Timeloom banner"
+          sourceLink="https://github.com/inclinedadarsh/timeloom"
+          liveLink="https://timeloom.vercel.app"
+          technologies={[
+            "Next.js",
+            "TypeScript",
+            "Node.js",
+            "MongoDB",
+            "TailwindCSS",
+            "UI/UX",
+          ]}
+        />
+        <ProjectCard
+          title="Ciphered"
+          scope="Design + Development"
+          description="An open-source web app that let's you encrypt or decrypt any message you want using various algorithms and keys."
+          image={cipheredBanner}
+          imageAlt="Ciphered banner"
+          sourceLink="https://github.com/inclinedadarsh/ciphered"
+          liveLink="https://ciphered.vercel.app"
+          technologies={[
+            "Next.js",
+            "TypeScript",
+            "Node.js",
+            "TailwindCSS",
+            "UI/UX",
+          ]}
+        />
+        <ProjectCard
+          title="Unlinked"
+          scope="Design (Web)"
+          description="Unlinked hiring process fun and easier important information that a hiring manager actually need without wasting so much time."
+          image={unlinkedBanner}
+          imageAlt="Unlinked banner"
+          liveLink="https://www.behance.net/gallery/152416557/Website-for-hiring-developers-Hackathon-Winner"
+          technologies={["UI Design", "UX Design", "Figma"]}
+        />
+      </div>
+      <div className="mt-6 flex items-center gap-5">
+        <div className="grow border-t-2 border-muted-1 border-dashed"></div>
+        <Link href="/work" className="button-secondary">
+          See all projects
+        </Link>
+      </div>
+    </section>
   );
 };
 
