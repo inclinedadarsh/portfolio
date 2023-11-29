77424a61f098020aeff278ca393e21ab9ba590ff
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-11-29 22:05:55 +0530

    feat(meta-tags): :bento: Add metadata and opengraph image
    add favicon.ico, add opengraph-image.png and add og tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,20 @@ export const metadata: Metadata = {
   title: "Adarsh Dubey • Web Developer & UX Designer",
   description:
     "Adarsh Dubey is a freelance web developer and UX designer. He specializes in creating websites for startups.",
+  metadataBase: new URL("https://adarshdubey.com"),
+  openGraph: {
+    title: "Adarsh Dubey • Web Dev, UX Designer & ML Enthusiast",
+    description:
+      "Freelance web developer, UX designer, and ML enthusiast. Leading BIO-S Community, proud member of WeMakeDevs.",
+    url: "https://adarshdubey.com",
+    siteName: "Adarsh Dubey Portfolio Website",
+    images: {
+      url: "/opengraph-image.png",
+      width: 1920,
+      height: 960,
+      alt: "Adarsh Dubey: Web Dev, UX Designer and Machine Learning Enthusiast",
+    },
+  },
 };
 
 export default function RootLayout({
