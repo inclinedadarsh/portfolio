10717a4f23964a70f934e4ca71d8aae5da3fed7d
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-12-24 19:55:12 +0530

    feat: add banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
+
+interface BannerProps extends React.HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode;
+    className?: string;
+    url: string;
+    linkText: string;
+}
+
+const Banner = ({
+    className,
+    children,
+    linkText,
+    url,
+    ...props
+}: BannerProps) => {
+    return (
+        <div
+            className={`py-2 bg-primary flex flex-col md:flex-row gap-1.5 md:gap-3 items-center justify-center text-sm md:text-base ${className}`}
+            {...props}
+        >
+            <span className=''>{children}</span>
+            <Link
+                href={url}
+                className='flex gap-0.5 items-center underline hover:no-underline font-medium'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                {linkText} <ArrowUpRight size={20} />
+            </Link>
+        </div>
+    );
+};
+
+export default Banner;
