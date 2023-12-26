fd1d9f74cee6354f94589ded452baa5731411f6d
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2023-12-26 20:20:54 +0530

    feat: add banner component (#22)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,51 +4,57 @@ import { Space_Grotesk } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
+import Banner from "@/components/Banner";
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  variable: "--font-space-grotesk",
+    subsets: ["latin"],
+    variable: "--font-space-grotesk",
 });
 
 const generalSans = localFont({
-  src: "./fonts/GeneralSans-Variable.ttf",
-  display: "swap",
-  fallback: ["system-ui", "arial"],
-  variable: "--font-general-sans",
+    src: "./fonts/GeneralSans-Variable.ttf",
+    display: "swap",
+    fallback: ["system-ui", "arial"],
+    variable: "--font-general-sans",
 });
 
 export const metadata: Metadata = {
-  title: "Adarsh Dubey • Web Developer & UX Designer",
-  description:
-    "Adarsh Dubey is a freelance web developer and UX designer. He specializes in creating websites for startups.",
-  metadataBase: new URL("https://adarshdubey.com"),
-  openGraph: {
-    title: "Adarsh Dubey • Web Dev, UX Designer & ML Enthusiast",
+    title: "Adarsh Dubey • Web Developer & UX Designer",
     description:
-      "Freelance web developer, UX designer, and ML enthusiast. Leading BIO-S Community, proud member of WeMakeDevs.",
-    url: "https://adarshdubey.com",
-    siteName: "Adarsh Dubey Portfolio Website",
-    images: {
-      url: "/opengraph-image.png",
-      width: 1920,
-      height: 960,
-      alt: "Adarsh Dubey: Web Dev, UX Designer and Machine Learning Enthusiast",
+        "Adarsh Dubey is a freelance web developer and UX designer. He specializes in creating websites for startups.",
+    metadataBase: new URL("https://adarshdubey.com"),
+    openGraph: {
+        title: "Adarsh Dubey • Web Dev, UX Designer & ML Enthusiast",
+        description:
+            "Freelance web developer, UX designer, and ML enthusiast. Leading BIO-S Community, proud member of WeMakeDevs.",
+        url: "https://adarshdubey.com",
+        siteName: "Adarsh Dubey Portfolio Website",
+        images: {
+            url: "/opengraph-image.png",
+            width: 1920,
+            height: 960,
+            alt: "Adarsh Dubey: Web Dev, UX Designer and Machine Learning Enthusiast",
+        },
     },
-  },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${generalSans.variable} ${spaceGrotesk.variable}`}>
-        <Navigation />
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body
+                className={`${generalSans.variable} ${spaceGrotesk.variable}`}
+            >
+                <Banner linkText='Join the crew' url='https://bios.vercel.app/'>
+                    I'm looking for folks to work with me at BIO-S
+                </Banner>
+                <Navigation />
+                {children}
+                <Analytics />
+            </body>
+        </html>
+    );
 }
