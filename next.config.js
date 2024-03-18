5ec7234824403e019c14621270b3057d244f99ae
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-03-18 21:44:12 +0530

    feat: add config in nextconfig to redirect /blog to different repo page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://blog-self-six-88.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://blog-self-six-88.vercel.app/blog/:path*",
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
