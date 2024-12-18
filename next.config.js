842d13bc5d02eb1d1a953a6222724185f00deb02
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-12-18 19:53:22 +0530

    fix: change resume endpoints from rewrite to redirect

--- next.config.js
+++ next.config.js
@@ -10,20 +10,27 @@ const nextConfig = {
                 source: "/blog/:path*",
                 destination: "https://blog-self-six-88.vercel.app/blog/:path*",
             },
+        ];
+    },
+    async redirects() {
+        return [
             {
                 source: "/webdev",
                 destination:
                     "https://drive.google.com/file/d/1b-puCaHl8Ys9siaok7SZZL05IV94G4wY/view?usp=sharing",
+                permanent: true,
             },
             {
                 source: "/devrel",
                 destination:
                     "https://drive.google.com/file/d/1Mh833p8PMYZYOqwo-D-MEpSmcuohUHYH/view?usp=sharing",
+                permanent: true,
             },
             {
                 source: "/mle",
                 destination:
                     "https://drive.google.com/file/d/1hZnw-vQn671sOK-3psuq4ASV_PeMIg6i/view?usp=sharing",
+                permanent: true,
             },
         ];
     },
