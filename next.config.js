5cf8a04a7b16b9d78876947f4a7914c12a4932ca
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-12-18 19:39:59 +0530

    feat: redirect endpoints to resume - redirect /webdev to webdev resume - redirect /mle to mle resume - redirect /devrel to devrel resume

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
         source: "/blog/:path*",
         destination: "https://blog-self-six-88.vercel.app/blog/:path*",
       },
+      {
+        source: "/webdev",
+        destination: "https://drive.google.com/file/d/1b-puCaHl8Ys9siaok7SZZL05IV94G4wY/view?usp=sharing"
+      },
+      {
+        source: "/devrel",
+        destination: "https://drive.google.com/file/d/1Mh833p8PMYZYOqwo-D-MEpSmcuohUHYH/view?usp=sharing"
+      },
+      {
+        source: "/mle",
+        destination: "https://drive.google.com/file/d/1hZnw-vQn671sOK-3psuq4ASV_PeMIg6i/view?usp=sharing"
+      },
     ];
   },
 }
